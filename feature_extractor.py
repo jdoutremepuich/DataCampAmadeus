475f3e865955a41281b91595597de43ddb28df9e
jdoutremepuich <jdoutremepuich@gmail.com>
2015-07-03 16:02:12 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -34,10 +34,10 @@ class FeatureExtractor(object):
         X_encoded['n_days'] = X_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
         
         #X_encoded = X_encoded.join(pd.get_dummies(X_encoded['year'], prefix='y'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['day'], prefix='d'))
+        #X_encoded = X_encoded.join(pd.get_dummies(X_encoded['month'], prefix='m'))
+        #X_encoded = X_encoded.join(pd.get_dummies(X_encoded['day'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['weekday'], prefix='wd'))
-        #X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
+        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['week'], prefix='w'))
         
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
