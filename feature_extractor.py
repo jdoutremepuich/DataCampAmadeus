22ec713390234a83644e6c041b1472a17e1ca0c0
jdoutremepuich <jdoutremepuich@gmail.com>
2015-07-04 16:20:50 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -19,8 +19,6 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.merge(special_days, how='left', left_on=['DateOfDeparture'], right_on=['DateOfDeparture'], sort=False)
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop('Departure', axis=1)
-        X_encoded = X_encoded.drop('Arrival', axis=1)     
         
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
@@ -37,5 +35,7 @@ class FeatureExtractor(object):
         X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
         X_encoded = X_encoded.drop('ISSPECIALDAY', axis=1)
         X_encoded = X_encoded.drop('ISAROUNDSPECIALDAY', axis=1)
+        X_encoded = X_encoded.drop('Departure', axis=1)
+        X_encoded = X_encoded.drop('Arrival', axis=1)  
         X_array = X_encoded.values
         return X_array
